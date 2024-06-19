8bb6e2c8dc95c0b0ba0bf197e61f20fcae20360a
Luannt <luan.nt295@gmail.com>
2024-06-19 19:59:04 +0700

    xóa file cũ

--- cau05.py
+++
@@ -1,7 +0,0 @@
-#Viết chương trình nhập vào từ bàn phím một số bất kỳ ở hệ thập phân và hiển thị ra màn hình số đó ở hệ bát phân (Có xử lý ngoại lệ đầu vào).
-so = input('Nhập 1 số: ')
-try:
-    sothapphan = int(so)
-    print(' Số thập phân %d có số bát phân là %o' %(sothapphan,sothapphan) )
-except:
-    print('Nhập sai giá trị')
